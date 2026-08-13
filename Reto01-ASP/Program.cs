68c34602b94bf10ad78018f6024510478eeb0d76
Kevin Rodriguez <kevin.rodriguez5@catolica.edu.sv>
2026-08-13 17:49:24 -0600

    22 PRIMERA MIGRACIÓN

--- Reto01-ASP/Program.cs
+++ Reto01-ASP/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
