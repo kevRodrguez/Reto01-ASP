71f89ce61e51137fe1a765a1f995ccd9b89da4b6
Kevin Rodriguez <kevin.rodriguez5@catolica.edu.sv>
2026-08-13 16:59:43 -0600

    nugget package

--- Reto01-ASP/obj/Debug/net10.0/Reto01-ASP.AssemblyInfo.cs
+++ Reto01-ASP/obj/Debug/net10.0/Reto01-ASP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reto01-ASP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c722ecb3082c9492d63345ba0f5322cef1ed96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reto01-ASP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reto01-ASP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
