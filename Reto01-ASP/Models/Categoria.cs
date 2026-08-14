68f075d25c811dcaff427f6f8fdcde3cde4e14d7
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2026-08-13 18:01:21 -0600

    28-crear categoria

---
+++ Reto01-ASP/Models/Categoria.cs
@@ -0,0 +1,8 @@
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Descripcion { get; set; }
+    public ICollection<Producto> Productos { get; set; }
+}
